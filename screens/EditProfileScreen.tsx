36352be7e169a70e517ac25629bd80e811175bff
Laura Toft <laur507p@stud.kea.dk>
2022-05-15 21:51:34 +0200

    Add edit profile

--- screens/EditProfileScreen.tsx
+++ screens/EditProfileScreen.tsx
@@ -5,13 +5,16 @@ import InputField from "../components/InputField";
 
 export default function EditProfileScreen({}) {
   const [inputValid1, setInputValid1] = useState(false);
+  const [inputValid2, setInputValid2] = useState(false);
+
   const user = useSelector((state) => state.user);
   const [text, setText] = useState(user.username);
-
+  const [text2, setText2] = useState(user.username);
   console.log(inputValid1, text);
 
   return (
     <>
+      <Text>Is form valid? {inputValid1 && inputValid2 ? "Yes" : "No"}</Text>
       <Text>Edit Profile</Text>
       <InputField
         isValid={inputValid1}
@@ -21,6 +24,16 @@ export default function EditProfileScreen({}) {
         label="Input field"
         text={text}
         setText={setText}
+      />
+
+      <InputField
+        isValid={inputValid2}
+        setIsValid={setInputValid2}
+        placeholder="Input field 2"
+        errorMessage="Invalid name"
+        label="Input field"
+        text={text2}
+        setText={setText2}
       />
     </>
   );
