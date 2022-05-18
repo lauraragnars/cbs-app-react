f02cc3b01ee4ca0c34663aeb8d55850b6aee3f22
Andrea Valgeirsdóttir <andreavalgeirsdottir@Andreas-MacBook-Pro-2.local>
2022-05-18 15:27:06 +0200

    repeat password field added

--- src/styles/General.tsx
+++ src/styles/General.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet } from "react-native";
-import { variables } from "./Variables";
+import { StyleSheet } from 'react-native';
+import { variables } from './Variables';
 
 export const general = StyleSheet.create({
   background: {
@@ -8,6 +8,8 @@ export const general = StyleSheet.create({
     color: variables.colors.blue300
   },
   padding: {
-    padding: 15
+    paddingTop: 50,
+    paddingLeft: 15,
+    paddingRight: 15
   }
 });
